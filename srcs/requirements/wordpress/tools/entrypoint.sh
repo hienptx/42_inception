96aaa77ab25e0d9164e6a98b62e162122f4dfa63
mafalda <hienxuan93@gmail.com>
2025-07-28 14:13:35 +0200

    gengerate certs keys with Dockerfile

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Wait for MariaDB to be ready
 echo "Waiting for database..."
-until mysqladmin ping -h"$MYSQL_HOST" --silent; do
+until mysqladmin ping -h"$DB_HOST" --silent; do
   sleep 1
 done
 
@@ -11,8 +11,17 @@ done
 if [ ! -f /var/www/html/wp-config.php ]; then
   echo "Setting up WordPress..."
   wp core download --allow-root
-  wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
-  wp core install --url="https://$DOMAIN_NAME" --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
+  wp config create  --dbname=$DB_DATABASE \
+                    --dbuser=$DB_USER \
+                    --dbpass=$DB_PASSWORD \
+                    --dbhost=$DB_HOST \
+                    --allow-root
+  wp core install   --url="https://$DOMAIN_NAME" \
+                    --title=$WP_TITLE \
+                    --admin_user=$WP_ADMIN \
+                    --admin_password=$WP_ADMIN_PASS \
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --allow-root
 fi
 
 exec "$@"
